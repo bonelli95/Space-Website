fdce889150b89626cb1130ea726250e4ba0a468a
bonelli95 <rodrigobonelli95@gmail.com>
2024-03-07 08:19:20 +0000

    iterando o projeto

---
+++ gallery/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+from gallery.models import Fotografia
+
+def index(request):
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+    return render(request, 'gallery/index.html', {'cards': fotografias})
+
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'gallery/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render(request, 'gallery/buscar.html', {'cards': fotografias})
